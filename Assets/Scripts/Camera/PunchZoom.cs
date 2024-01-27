ce4e3931d3b14a4a44f4f82cd4c6a8b5ab419c83
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-01-27 11:52:07 +0100

    stuff

--- Assets/Scripts/Camera/PunchZoom.cs
+++ Assets/Scripts/Camera/PunchZoom.cs
@@ -1,18 +1,19 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PunchZoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    GameObject _player;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    CinemachineVirtualCamera _camera;
+
+    public void Zoom()
     {
-        
+        Time.timeScale = 0;
+        _camera.m_Lens.FieldOfView = Mathf.Lerp(60, 30, 0.5f);
     }
 }
